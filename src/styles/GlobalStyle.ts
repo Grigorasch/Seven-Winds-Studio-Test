09047afbdf93f4dd2f3cb6db2ccf21a46a58e4bf
Grigorasch <pgv856@gmail.com>
2023-12-28 06:27:22 +0600

    Настроены глобальные параметры

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html, body, div, span, h1, h2, h3, p,
+    a, img,  ul, li,form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, details, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        box-sizing: border-box;
+        font-size: 100%;
+        font: inherit;
+        color: inherit;
+        vertical-align: baseline;
+    }
+    /* HTML5 display-role reset for older browsers */
+    article, aside, details, figcaption, figure, 
+    footer, header, hgroup, menu, nav, section {
+        display: block;
+    }
+    body {
+        font-family: 'Roboto', Arial, sans-serif;
+    }
+    ol, ul {
+        list-style: none;
+    }
+    blockquote, q {
+        quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+`
---
+++ src/styles/colorTheme.ts
@@ -0,0 +1,11 @@
+export const colorTheme = {
+    frameColor: '#414144',
+
+    lightTextColor: '#FFFFFF',
+    darkTextCollor: '#A1A1AA',
+
+    menuBackgroundColor: '#27272A',
+    mainBackgroundColor: '#202124',
+
+    sectionButtonHoverColor: '#A1A1AA',
+}
